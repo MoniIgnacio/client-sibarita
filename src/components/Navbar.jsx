0cdc9455144ed318d9b9eeda1de774c12fcba787
MoniIgnacio <moniignacio02@gmail.com>
2022-11-12 17:39:00 +0400

    add button Close Session & function remove token from localStorage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import {useContext} from 'react'
 import { Link } from 'react-router-dom'
 import LoginModal from './LoginModal'
+import {AuthContext} from "../components/context/auth.context"
+
 
 function Navbar() {
+
+  const { authenticaUser, isLoggedIn, setUser, setIsLoggedIn } = useContext(AuthContext)
+
+
+  const handleLogout = () => {
+    localStorage.removeItem("authToken")
+    authenticaUser()
+  }
   return (
     <div>
     <LoginModal/>
     <Link to={'/'}>Home</Link>
     <Link to={'/signup'}>Sign Up</Link>
     <Link to={"/restaurant/create"}>Da de alta tu restaurante</Link>
+    <button onClick={handleLogout}>Cerrar Sesión</button>
+
     </div>
   )
 }
