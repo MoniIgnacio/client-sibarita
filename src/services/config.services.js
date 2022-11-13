bb454acff5aa2966ebea43d07b2fabf1314ddb05
MoniIgnacio <moniignacio02@gmail.com>
2022-11-13 16:07:46 +0400

    fixing some token bugs

--- src/services/config.services.js
+++ src/services/config.services.js
@@ -4,4 +4,19 @@ const service = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL
 });
 
+service.interceptors.request.use((config)=>{
+
+  const authToken = localStorage.getItem('authToken');
+
+  const tokenFull = `Bearer ${authToken}`
+
+  if ( authToken) {
+    config.headers.authorization = tokenFull
+  }
+
+  return config
+})
+
+
+
 export default service;
