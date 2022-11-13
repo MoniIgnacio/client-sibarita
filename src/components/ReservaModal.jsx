8a037d68aa2f6fbf7716765eab85039b9731fb1f
Jelle <jmanzanohaagsma@gmail.com>
2022-11-13 11:49:46 +0100

    Worked on reserve modal form

--- src/components/ReservaModal.jsx
+++ src/components/ReservaModal.jsx
@@ -1,9 +1,113 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { createReservaService } from "../pages/services/restaurant.services";
 
 function ReservaModal() {
+  const navigate = useNavigate();
+  const [fecha, setFecha] = useState("");
+  const [hour, setHour] = useState("");
+  const [pax, setPax] = useState(1);
+  const [hasConsumed, setHasConsumed] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleFechaChange = (e) => setFecha(e.target.value);
+  const handleHourChange = (e) => setHour(e.target.value);
+  const handlePaxChange = (e) => setPax(e.target.value);
+
+  const handleReserve = async (e) => {
+    e.preventDefault();
+
+    const newReserve = {
+      fecha: fecha,
+      hour: hour,
+      pax: pax,
+      hasConsumed: hasConsumed,
+    };
+
+    try {
+      await createReservaService(newReserve);
+      //! cambiar navigate a vista detallada del restaurante
+      navigate("/home");
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        setErrorMessage(error.response.data.errorMessage);
+      } else {
+        navigate("/error");
+      }
+    }
+  };
   return (
-    <div>ReservaModal</div>
-  )
+    <div>
+      <Button variant="primary" onClick={handleShow}>
+        Haz tu reserva
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Cerrar</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {" "}
+          <Form onSubmit={handleReserve}>
+            <fieldset>
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="disabledTextInput">Día:</Form.Label>
+                <Form.Control
+                  id="disabledTextInput"
+                  type="date"
+                  name="fecha"
+                  value={fecha}
+                  onChange={handleFechaChange}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="disabledTextInput">Hora:</Form.Label>
+                <Form.Control
+                  id="disabledTextInput"
+                  type="date"
+                  name="hour"
+                  value={hour}
+                  onChange={handleHourChange}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="disabledTextInput">
+                  Nº de comensales:
+                </Form.Label>
+                <Form.Control
+                  id="disabledTextInput"
+                  type="number"
+                  name="pax"
+                  value={pax}
+                  onChange={handlePaxChange}
+                />
+              </Form.Group>
+
+              {errorMessage !== "" && <p>{errorMessage}</p>}
+            </fieldset>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+          <Button variant="primary" onClick={handleReserve}>
+            Reservar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
 
-export default ReservaModal
+export default ReservaModal;
