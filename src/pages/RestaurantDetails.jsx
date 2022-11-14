7196cabfd858563c698cb95b403056ce6796169d
Jelle <jmanzanohaagsma@gmail.com>
2022-11-14 12:30:54 +0100

    bug fixing restaurant details

--- src/pages/RestaurantDetails.jsx
+++ src/pages/RestaurantDetails.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { getRestaurantService } from "../services/restaurant.services";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 function RestaurantDetails() {
   const navigate = useNavigate();
-  const [details, setDetails] = useState();
+  const [details, setDetails] = useState("");
   const [isFetching, setIsFetching] = useState(true);
-
+  const { restId } = useParams();
   useEffect(() => {
     getRestaurant();
   }, []);
 
   const getRestaurant = async () => {
     try {
-      let response = await getRestaurantService();
+      let response = await getRestaurantService(restId);
       setDetails(response.data);
       setIsFetching(false);
-      console.log(response.data);
+      console.log("ejemplo de objeto",response.data);
     } catch (error) {
       console.log(error);
       navigate("/error");
@@ -26,19 +26,13 @@ function RestaurantDetails() {
   if (isFetching) {
     return <h3>Loading... </h3>;
   }
+
   return (
     <div>
-    <h3>Hola</h3>
-       {/* {details.map((eachRestaurant) => {
-        return (
-          <div key={eachRestaurant._id}>
-            <h1>{eachRestaurant.name}</h1>
-            <h3>{eachRestaurant.location}</h3>
-            <h5>{eachRestaurant.cuisinType}</h5>
-            <h6>{eachRestaurant.phoneNumber}</h6>
-          </div>
-        );
-      })}  */}
+      <h1>{details.name}</h1>
+      <h3>{details.location}</h3>
+      <h5>{details.cuisinType}</h5>
+      <h6>{details.phoneNumber}</h6>
     </div>
   );
 }
