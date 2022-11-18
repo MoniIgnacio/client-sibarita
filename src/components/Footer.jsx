7a0c582b416918bd7d0f5f72c4d75b51da4121a0
MoniIgnacio <moniignacio02@gmail.com>
2022-11-18 18:12:01 +0400

    add video link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
       <li className="menu__item"><a className="menu__link" href="https://github.com/MoniIgnacio/client-sibarita"><img src="https://res.cloudinary.com/dbrqv6ypj/image/upload/v1668759559/Sibarita-img/khtqyiuurkezv6apovbs.png" alt="img-git" width={'30px'} /></a></li>
       <li className="menu__item"><a className="menu__link" href="https://www.linkedin.com/in/jelle-manzano-haagsma-6a6785189/">Jelle <img src="https://res.cloudinary.com/dbrqv6ypj/image/upload/v1668760275/Sibarita-img/unhhwokhmybg5txkwjvk.png" alt="link-img" width={'15px'} style={{background:'white'}}/></a></li>
       <li className="menu__item"><a className="menu__link" href="https://www.linkedin.com/in/moniignacio02">Nacho <img src="https://res.cloudinary.com/dbrqv6ypj/image/upload/v1668760275/Sibarita-img/unhhwokhmybg5txkwjvk.png" alt="link-img" width={'15px'} style={{background:'white'}}/></a></li>
-      <li className="menu__item"><a className="menu__link" href="#">Contact</a></li>
+      <li className="menu__item"><a className="menu__link" href="https://drive.google.com/file/d/1DkO-J1p9rzbsSe1dQ77Iq3LnOvbVnR6n/view">Proyect video</a></li>
 
     </ul>
     <p>&copy; 2022 Jelle & Nacho | All Rights Reserved</p>
